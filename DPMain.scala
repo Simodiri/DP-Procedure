696e72f59826d386ccf400314c2dec09cf0e91f5
Simodiri <85987994+Simodiri@users.noreply.github.com>
2021-09-26 18:36:02 +0200

    Update DPMain.scala

--- DPMain.scala
+++ DPMain.scala
@@ -1,7 +1,6 @@
 object DPMain extends App{
 	var f:Formula = Parser.getFormula
 	println("Formula in input: " +f)
-	
 	var res = Solver.DPLL(f)
 	if(res._1){
 		println("Soddisfacibile con DPLL")
@@ -10,14 +9,12 @@ object DPMain extends App{
 	else{
 		println("Non soddisfacibile con DPLL.")
 	}
-	
 	var res3 = Solver.DP(f)
         if(res3){
 		println("Soddisfacibile con Davis Putnam")
 	}else{
 		println("Non soddisfacibile con DP")
 	} 
-	
 	def printa(assign:List[(String,Boolean)],f:Formula):String={
 		var stringa=new String()
 		assign.map(elem=>elem._1+":"+elem._2+",").reduce(_+_).dropRight(1)
