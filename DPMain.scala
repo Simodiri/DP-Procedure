b56b6eba26fe16eb6485ec102e559e1930d48a05
Simodiri <85987994+Simodiri@users.noreply.github.com>
2021-09-26 18:33:40 +0200

    Update DPMain.scala

--- DPMain.scala
+++ DPMain.scala
@@ -1,26 +1,26 @@
 object DPMain extends App{
-	//var f:Formula = new Formula(Set(U(1), C(Set(1,2,-3)), C(Set(-1,2,3,4)), C(Set(-5,-1)), C(Set(-5,-3)), U(4))) (soddisfacibile)
-	//var f: Formula = new Formula(Set(C(Set(1,-2,4)), C(Set(-2,3,4)), C(Set(-1,2,-3,-4)), C(Set(2,-3))))//soddisfacibile (fa anche chiamata ricorsiva)
-
 	var f:Formula = Parser.getFormula
-	println(f)
+	println("Formula in input: " +f)
 	
-	var res = Performance.profila(Solver.DPLL(f))
-	if(res._1._1){
-		println("Soddisfacibile con DPLL (Ha impiegato " + res._2*1E-9 + " sec).")
-		println(res._1._2)
+	var res = Solver.DPLL(f)
+	if(res._1){
+		println("Soddisfacibile con DPLL")
+		println("Assegnazioni: "+printa(res._2,f))
 	}
 	else{
-		println("Non soddisfacibile.")
+		println("Non soddisfacibile con DPLL.")
 	}
 	
 	var res3 = Solver.DP(f)
-    if(res3._1){
+        if(res3){
 		println("Soddisfacibile con Davis Putnam")
-		println(res3._2)
-	}
-	else{
+	}else{
 		println("Non soddisfacibile con DP")
 	} 
 	
+	def printa(assign:List[(String,Boolean)],f:Formula):String={
+		var stringa=new String()
+		assign.map(elem=>elem._1+":"+elem._2+",").reduce(_+_).dropRight(1)
+	}
 }
+ 
